2e623aec52e31d4da121476ea6b3ae4c1c291665
Saad982 <sheikhsaad671@gmail.com>
2025-07-24 14:52:19 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PlantPal',
+  webDir: 'www'
+};
+
+export default config;
